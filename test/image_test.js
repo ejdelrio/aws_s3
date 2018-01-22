42e49d8774e2983496c6fc9c42cf603ea085360e
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-01-22 15:31:23 -0800

    Created test file for image resource and image routes

---
+++ test/image_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const superagent = require('superagent');
+
+require('../server.js');
+
+describe('Test for image resource\'s API Routes', function () {
+
+});
