b9db79d2e31c8dba6c3a69af7a2319bf3aaf06fd
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-01-22 16:09:58 -0800

    Created Image Router with basic get route

---
+++ routes/image-router.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const Router = require('express').Router;
+const createError = require('http-errors');
+const debug = require('debug')('aws: Image Router');
+const jsonParser = require('body-parser').json();
+
+const Image = require('../model/image.js');
+
+const imageRouter = module.exports = new Router();
+
+imageRouter.get('/api/image/', function(req, res, next) {
+  debug('GET /api/image');
+
+  Image.find({})
+  .then(imageArr => res.json(imageArr))
+  .catch(err => next(createError(400, err)));
+});
