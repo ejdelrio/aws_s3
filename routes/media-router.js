ad454d9b56e06c8be43a2df52125935d57651c71
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-01-23 16:05:33 -0800

    Wrote function to process AWS parameters and construct AWS request :D

--- routes/media-router.js
+++ routes/media-router.js
@@ -4,6 +4,27 @@ const Router = require('express').Router;
 const createError = require('http-errors');
 const debug = require('debug')('aws: Image Router');
 const jsonParser = require('body-parser').json();
+const path = require('path');
+const del = require('del');
+const fs = require('fs');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const s3 = new AWS.S3();
+const dataDir = `${__dirname}/../data`;
+const upload = multer({ dest: dataDir });
+
+function s3Prom(params) {
+  return new Promise((resolve, reject) => {
+    console.log('processing')
+    s3.upload(params, (err, s3data) => {
+      if(err) console.log(err);
+      resolve(s3data);
+    });
+  });
+}
 
 const Media = require('../models/media.js');
 
