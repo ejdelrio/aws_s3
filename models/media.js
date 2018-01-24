3c5384588224e100c6112223c3f4129e8a222b2d
Eddie <Eddie@Edwins-MacBook-Pro.local>
2018-01-23 16:29:57 -0800

    Added majority of post request for aws upload

--- models/media.js
+++ models/media.js
@@ -7,8 +7,7 @@ require('debug')('aws: Media Resource');
 const mediaSchema = new Schema({
   caption: {type: String, required: false},
   url: {type: String, unique: true, required: true},
-  fileType: {type: String, required: true, unique: false},
-  size: {type: Number, required: true, unique: false},
+  key: {type: String, required: true},
   dateUploaded: {type: Date, default: Date.now},
   mediaType: {type: String, required: true, unique: false}
 });
--- routes/media-router.js
+++ routes/media-router.js
@@ -18,9 +18,9 @@ const upload = multer({ dest: dataDir });
 
 function s3Prom(params) {
   return new Promise((resolve, reject) => {
-    console.log('processing')
+    console.log('processing');
     s3.upload(params, (err, s3data) => {
-      if(err) console.log(err);
+      if(err) reject(err);
       resolve(s3data);
     });
   });
@@ -37,11 +37,40 @@ mediaRouter.get('/api/:mediaType/', function(req, res, next) {
 
   Media.find({mediaType})
   .then(mediaArr => res.json(mediaArr))
+  .then(next)
   .catch(err => next(createError(400, err)));
 });
 
-mediaRouter.post('/api/:mediaType/', jsonParser, function(req, res, next) {
+mediaRouter.post('/api/:mediaType/', jsonParser, upload.single('media'), function(req, res, next) {
   debug('POST /api/:mediaType/');
 
+  if (!req.file) return next(createError(400, 'no file found'));
+  if (!req.file.path) return next(createError(500, 'no file saved'));
+
+  let ext = path.extname(req.file.originalname);
+
+  let params = {
+    ACL: 'public-read',
+    Bucket: process.env.AWS_BUCKET,
+    Key: `${req.file.filename}.${ext}`,
+    Body: fs.createReadStream(req.file.path)
+  };
+
+  s3Prom(params)
+  .then(data => {
+
+    del([`${dataDir}/*`]);
+    let uploadBody = {
+      caption: req.body.caption,
+      awsKey: data.Key,
+      awsURI: data.Location,
+      mediaType: req.params.mediaType
+    };
+    return uploadBody;
+  })
+  .then(body => new Media(body).save())
+  .then(mediaUpload => res.json(mediaUpload))
+  .then(next)
+  .catch(err => next(createError(400, err.message)));
 
 });
